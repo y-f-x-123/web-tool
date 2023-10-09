3dc33097bad766b784826f00d4a5f46cdd8cbbff
yfx@china315net.com <yfx@china315net.com>
2023-10-09 16:12:41 +0800

    刷新404

--- vite.config.js
+++ vite.config.js
@@ -35,6 +35,9 @@ export default defineConfig({
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
   },
+  build: {
+    assetsPublicPath: './assets' //此处为修改的地方，这里加.
+  },
   server: {
     host: 'localhost', // 指定服务器主机名
     port: 4090, // 指定服务器端口
