db98338082ffb44bc70b809b7ea4c7695b244588
yfx@china315net.com <yfx@china315net.com>
2023-12-01 09:54:52 +0800

    其他工具

--- src/module/tools/files/其他工具.js
+++ src/module/tools/files/其他工具.js
@@ -6,6 +6,11 @@ export default {
       icon: 'https://nvm.uihtm.com/favicon.ico',
       name: 'NVM-node版本管理',
       link: 'https://nvm.uihtm.com/'
+    },
+    {
+      icon: 'https://nodejs.org/static/images/favicons/favicon.png',
+      name: 'node版本',
+      link: 'https://nodejs.org/dist/'
     }
   ]
 }
